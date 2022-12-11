c52242e30386f771013c179b4aee7a2748109f34
Omar Omar <oomar@umich.edu>
2022-12-10 21:46:35 -0800

    Add Barometer

--- arduino-433-transmitter.ino
+++ arduino-433-transmitter.ino
@@ -1,8 +1,11 @@
 #define RCSwitchDisableReceiving
 #include <RCSwitch.h>
+#include <Adafruit_MPL3115A2.h>
 #include <pins_arduino.h>
+#include <cmath>
 
 RCSwitch mySwitch{};
+Adafruit_MPL3115A2 baro;
 
 const int protocolNum = 6;
 const int repeatedTransmit = 16;
@@ -19,13 +22,36 @@ void setup() {
 
   mySwitch.setProtocol(protocolNum);
   mySwitch.setRepeatTransmit(repeatedTransmit);
+
+  // Setup Barometer.
+  if (!baro.begin()) {
+    Serial.println("Could not find sensor. Check wiring.");
+    while(true) {
+      // Inidacte issue with LEDs.
+      digitalWrite(LED_BUILTIN, LOW);
+      delay(100);
+      digitalWrite(LED_BUILTIN, HIGH);
+      delay(100);
+    }
+  }
+
+  // Since we only care about altitude in order to be mapped to
+  // parking floors, adjust the atlitude offset to not get negative values.
+  // Currently we are roughly -30m in altitude.
+  baro.setAltitudeOffset(50);
 }
 
 
 void loop() {
+  // Get barometer sensor info.
+  // float pressure = baro.getPressure(); // hPa
+  // float temperature = baro.getTemperature(); // C
+  float altitude = baro.getAltitude(); // m
+
+
   Serial.printf("[%lu] Sending code using protocol %d and repeats %d.\n", micros(), protocolNum, repeatedTransmit);
-  Serial.println("  Sending 69");
-  mySwitch.send(69, 24);
+  Serial.printf("Sending adjusted (+50m offset) altitude = %.2fm\n", altitude);
+  mySwitch.send(round(altitude), 24);
 
   digitalWrite(LED_BUILTIN, LOW);
   delay(1000);
